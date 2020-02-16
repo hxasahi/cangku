b826cc9aea0408bff9e30f115afa6b13d3c7d281
hxasahi <3287039936@qq.com>
2020-02-16 18:33:21 +0800

    code

---
+++ ceshi.py
@@ -0,0 +1,12 @@
+# -*- coding: cp936 -*-
+tianjia=[]
+print('添加ID:')
+zancun=int(input())
+tianjia.append(zancun)
+print('添加名称')
+zancun=raw_input()
+tianjia.append(zancun)
+print('添加价格')
+zancun=int(input())
+tianjia.append(zancun)
+print(tianjia)
---
+++ ceshi1.py
@@ -0,0 +1,4 @@
+# -*- coding: cp936 -*-
+i = raw_input('23423 fds')
+print (i)
+
